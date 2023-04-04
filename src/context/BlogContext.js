f6d74ed858e31d7ca2fb3400b126e85dfbd1d111
Chandler Ocapan <Cocapan@gmail.com>
2023-04-04 16:10:41 -0700

    Implemented reducer to add blog post

---
+++ src/context/BlogContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from 'react';
+
+const BlogContext = createContext();
+
+const blogReducer = (state, action) => {
+  console.log('state : ');
+  console.log(state);
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
